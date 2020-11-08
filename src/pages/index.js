262edcba7c998983aeae0b6d42124ed2341cbb4c
Oskar Wändesjö <oskar.wandesjo@gmail.com>
2020-11-08 18:38:19 +0100

    Deployment setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,14 @@
 import React from "react"
+import { Link } from "gatsby"
+import Header from "../components/header"
+import Footer from "../components/footer"
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return (
+    <div style={{ align: 'center'}}>
+      <Header />
+      <h1 align={"center"}>Oskar Wändesjö</h1>
+      <Footer />
+    </div>
+  )
 }
